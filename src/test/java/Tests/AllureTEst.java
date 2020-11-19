ffa2464697a37fc964ce29f360bcc3d4b73ff2e6
Morozov Ilya <01morozov92@gmail.com>
2020-11-19 19:55:04 +0300

    first test

--- src/test/java/Tests/AllureTEst.java
+++ src/test/java/Tests/AllureTEst.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class AllureTEst extends Utils {
 
-    private SelenideElement button = $(By.xpath("/html/body/div[2]/div[2]/div[3]/div/div[3]/ftgr/div/div/div[2]/div[1]/a/div"));
+    private SelenideElement button = $(By.xpath("/html/body/div[1]/div[2]/div[3]/div/div[3]/dhtaq/edvrt/aqwf/div/div/div[2]/div[1]/a/div"));
     private SelenideElement imagesButton = $(By.xpath("/html/body/div[1]/div[2]/div[3]/div/div[2]/div/div/div[2]/a[2]/div[1]"));
     private SelenideElement searchButton = $(By.xpath("/html/body/header/div/div[2]/div[1]/form/div[2]/button/div"));
     Steps steps = new Steps();
