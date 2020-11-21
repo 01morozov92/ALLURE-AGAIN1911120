b954733924ad060f03924575d5bc229e7d80db4a
Morozov Ilya <01morozov92@gmail.com>
2020-11-21 20:47:30 +0300

    first test

--- src/test/java/Utils/Utils.java
+++ src/test/java/Utils/Utils.java
@@ -1,6 +1,7 @@
 package Utils;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
@@ -10,6 +11,7 @@ public class Utils {
 
     @BeforeClass
     public static void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         Configuration.reportsFolder = "target/allure-results";
         Configuration.screenshots = false;
         Configuration.headless = true;
